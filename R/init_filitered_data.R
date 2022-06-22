767eaaf4844f17ac5dce4bb3b45774b73cbf9802
kpagacz <konrad.pagacz@gmail.com>
2022-06-22 10:57:09 +0200

    added more tests for init_filtered_data.default

--- R/init_filitered_data.R
+++ R/init_filitered_data.R
@@ -95,7 +95,6 @@ init_filtered_data.default <- function(x, join_keys = NULL, code = NULL, cdisc =
   mapply(validate_dataset_args, x, names(x), MoreArgs = list(allowed_parent = cdisc))
   checkmate::assert_class(code, "CodeClass", null.ok = TRUE)
   checkmate::assert_class(join_keys, "JoinKeys", null.ok = TRUE)
-  checkmate::check_flag(cdisc)
   checkmate::check_flag(check)
 
   datasets <- if (cdisc) {
