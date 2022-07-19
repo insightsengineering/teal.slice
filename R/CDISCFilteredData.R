5e02d48b61f8f212c0c3377fbe81a53439beabdb
Nikolas Burkoff <nikolas.burkoff@roche.com>
2022-07-19 13:07:56 +0100

    simplify

--- R/CDISCFilteredData.R
+++ R/CDISCFilteredData.R
@@ -85,11 +85,12 @@ CDISCFilteredData <- R6::R6Class( # nolint
       if (length(parent_dataname) == 0) {
         super$get_call(dataname)
       } else {
-        keys <- character(0)
-        join_keys <- self$get_join_keys()
 
-        if(!is.null(join_keys) && !is.null(join_keys$get(parent_dataname, dataname))) {
+        join_keys <- self$get_join_keys()
+        if(!is.null(join_keys)) {
           keys <- join_keys$get(parent_dataname, dataname)
+        } else {
+          keys <- character(0)
         }
 
         parent_keys <- names(keys)
