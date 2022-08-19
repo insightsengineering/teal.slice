2f01117969521f25b1bf1bf2fb559e1cfbb32c00
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-08-19 15:29:15 +0200

    fix

--- R/filter_panel_api.R
+++ R/filter_panel_api.R
@@ -88,17 +88,22 @@ FilterPanelAPI <- R6::R6Class( # nolint
       invisible(NULL)
     },
     turn_off = function() {
-      if (length(self$get_filter_state())) private$filtered_data_cache = private$filtered_data
-      self$remove_all_filter_states()
+      if (length(self$get_filter_state())) {
+        private$cached_states <- self$get_filter_state()
+        self$remove_all_filter_states()
+      }
     },
     turn_on = function() {
-      cached <- private$filtered_data_cache$get_filter_state()
-      if (length(self$get_filter_state()) == 0 && length(cached) > 0) self$set_filter_state(cached)
+      if (length(self$get_filter_state()) == 0 &&
+          !is.null(private$cached_states) &&
+          length(private$cached_states) > 0) {
+        self$set_filter_state(private$cached_states)
+      }
     }
   ),
   ## __Private Methods ====
   private = list(
-    filtered_data = NULL
-    , filtered_data_cache = NULL
+    filtered_data = NULL,
+    cached_states = NULL
   )
 )
