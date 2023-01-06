26ea5eed7811a89e97408ae9a7f5373dacca8ead
Nikolas Burkoff <nikolas.burkoff@roche.com>
2023-01-06 09:15:24 +0000

    fix range slider

--- R/FilterStateRange.R
+++ R/FilterStateRange.R
@@ -155,21 +155,25 @@ RangeFilterState <- R6::R6Class( # nolint
           class = "filterPlotOverlayRange",
           plotOutput(ns("plot"), height = "100%")
         ),
-        teal.widgets::optionalSliderInput(
-          inputId = ns("selection"),
-          label = NULL,
-          min = pretty_range_inputs["min"],
-          max = pretty_range_inputs["max"],
-          # on filter init without predefined value select "pretty" (wider) range
-          value = isolate({
-            if (identical(private$choices, self$get_selected())) {
-              pretty_range_inputs[c("min", "max")]
-            } else {
-              self$get_selected()
-            }
-          }),
-          width = "100%",
-          step = pretty_range_inputs["step"]
+        div(
+          class = "filterRangeSlider",
+          style = "width:100%", # needed for bs4
+          teal.widgets::optionalSliderInput(
+            inputId = ns("selection"),
+            label = NULL,
+            min = pretty_range_inputs["min"],
+            max = pretty_range_inputs["max"],
+            # on filter init without predefined value select "pretty" (wider) range
+            value = isolate({
+              if (identical(private$choices, self$get_selected())) {
+                pretty_range_inputs[c("min", "max")]
+              } else {
+                self$get_selected()
+              }
+            }),
+            width = "100%",
+            step = pretty_range_inputs["step"]
+          )
         ),
         if (private$inf_count > 0) {
           checkboxInput(
