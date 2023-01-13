2cfac5c4ebbb7b1dfc0eabd41465436e4d338c63
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-01-13 13:24:24 +0100

    add unit tests

--- tests/testthat/test-FilteredData.R
+++ tests/testthat/test-FilteredData.R
@@ -245,76 +245,76 @@ testthat::test_that(
 )
 
 testthat::test_that("FilteredData$get_filter_state returns list identical to input with attributes",
-  code = {
-    utils::data(miniACC, package = "MultiAssayExperiment")
-    datasets <- FilteredData$new(
-      list(
-        iris = list(dataset = iris),
-        mtcars = list(dataset = mtcars),
-        mae = list(dataset = miniACC)
-      ),
-      join_keys = NULL
-    )
+                    code = {
+                      utils::data(miniACC, package = "MultiAssayExperiment")
+                      datasets <- FilteredData$new(
+                        list(
+                          iris = list(dataset = iris),
+                          mtcars = list(dataset = mtcars),
+                          mae = list(dataset = miniACC)
+                        ),
+                        join_keys = NULL
+                      )
 
-    fs <- list(
-      iris = list(
-        Sepal.Length = list(selected = c(5.1, 6.4), keep_na = TRUE, keep_inf = FALSE),
-        Species = list(selected = c("setosa", "versicolor"), keep_na = FALSE)
-      ),
-      mae = list(
-        subjects = list(
-          years_to_birth = list(selected = c(30, 50), keep_na = TRUE, keep_inf = FALSE),
-          vital_status = list(selected = "1", keep_na = FALSE),
-          gender = list(selected = "female", keep_na = TRUE)
-        ),
-        RPPAArray = list(
-          subset = list(ARRAY_TYPE = list(selected = "", keep_na = TRUE))
-        )
-      )
-    )
-    datasets$set_filter_state(state = fs)
-    attr(fs, "formatted") <- isolate(datasets$get_formatted_filter_state())
-    testthat::expect_identical(isolate(datasets$get_filter_state()), fs)
-  }
+                      fs <- list(
+                        iris = list(
+                          Sepal.Length = list(selected = c(5.1, 6.4), keep_na = TRUE, keep_inf = FALSE),
+                          Species = list(selected = c("setosa", "versicolor"), keep_na = FALSE)
+                        ),
+                        mae = list(
+                          subjects = list(
+                            years_to_birth = list(selected = c(30, 50), keep_na = TRUE, keep_inf = FALSE),
+                            vital_status = list(selected = "1", keep_na = FALSE),
+                            gender = list(selected = "female", keep_na = TRUE)
+                          ),
+                          RPPAArray = list(
+                            subset = list(ARRAY_TYPE = list(selected = "", keep_na = TRUE))
+                          )
+                        )
+                      )
+                      datasets$set_filter_state(state = fs)
+                      attr(fs, "formatted") <- isolate(datasets$get_formatted_filter_state())
+                      testthat::expect_identical(isolate(datasets$get_filter_state()), fs)
+                    }
 )
 
 testthat::test_that("FilteredData$get_filter_state returns list whose attribute is a character form of the list",
-  code = {
-    utils::data(miniACC, package = "MultiAssayExperiment")
-    datasets <- FilteredData$new(
-      list(
-        iris = list(dataset = iris),
-        mtcars = list(dataset = mtcars),
-        mae = list(dataset = miniACC)
-      ),
-      join_keys = NULL
-    )
+                    code = {
+                      utils::data(miniACC, package = "MultiAssayExperiment")
+                      datasets <- FilteredData$new(
+                        list(
+                          iris = list(dataset = iris),
+                          mtcars = list(dataset = mtcars),
+                          mae = list(dataset = miniACC)
+                        ),
+                        join_keys = NULL
+                      )
 
-    fs <- list(
-      iris = list(
-        Sepal.Length = list(selected = c(5.1, 6.4), keep_na = TRUE, keep_inf = FALSE),
-        Species = list(selected = c("setosa", "versicolor"), keep_na = FALSE)
-      ),
-      mae = list(
-        subjects = list(
-          years_to_birth = list(selected = c(30, 50), keep_na = TRUE, keep_inf = FALSE),
-          vital_status = list(selected = "1", keep_na = FALSE),
-          gender = list(selected = "female", keep_na = TRUE)
-        ),
-        RPPAArray = list(
-          subset = list(ARRAY_TYPE = list(selected = "", keep_na = TRUE))
-        )
-      )
-    )
-    datasets$set_filter_state(state = fs)
-    formatted_attr <- isolate(datasets$get_formatted_filter_state())
+                      fs <- list(
+                        iris = list(
+                          Sepal.Length = list(selected = c(5.1, 6.4), keep_na = TRUE, keep_inf = FALSE),
+                          Species = list(selected = c("setosa", "versicolor"), keep_na = FALSE)
+                        ),
+                        mae = list(
+                          subjects = list(
+                            years_to_birth = list(selected = c(30, 50), keep_na = TRUE, keep_inf = FALSE),
+                            vital_status = list(selected = "1", keep_na = FALSE),
+                            gender = list(selected = "female", keep_na = TRUE)
+                          ),
+                          RPPAArray = list(
+                            subset = list(ARRAY_TYPE = list(selected = "", keep_na = TRUE))
+                          )
+                        )
+                      )
+                      datasets$set_filter_state(state = fs)
+                      formatted_attr <- isolate(datasets$get_formatted_filter_state())
 
-    testthat::expect_type(formatted_attr, "character")
-    testthat::expect_identical(
-      attr(isolate(datasets$get_filter_state()), "formatted"),
-      formatted_attr
-    )
-  }
+                      testthat::expect_type(formatted_attr, "character")
+                      testthat::expect_identical(
+                        attr(isolate(datasets$get_filter_state()), "formatted"),
+                        formatted_attr
+                      )
+                    }
 )
 
 testthat::test_that("FilteredData$remove_filter_state removes states defined in list", {
@@ -639,3 +639,55 @@ testthat::test_that("get_filter_panel_ui_id - non-empty when in shiny session", 
     }
   )
 })
+
+testthat::test_that(
+  "FilteredData$get_active_filtersproperly tallies active filter states",
+  code = {
+    datasets <- FilteredData$new(
+      list(
+        iris = list(dataset = iris),
+        mtcars = list(dataset = mtcars),
+        mae = list(dataset = miniACC)
+      ),
+      join_keys = NULL
+    )
+    fs <- list(
+      iris = list(
+        Sepal.Length = list(c(5.1, 6.4)),
+        Species = c("setosa", "versicolor")
+      ),
+      mtcars = list(
+        cyl = c(4, 6),
+        disp = list()
+      )
+    )
+    testthat::expect_equal(datasets$.__enclos_env__$private$get_filter_count(), 0L)
+    datasets$set_filter_state(state = fs)
+    testthat::expect_equal(datasets$.__enclos_env__$private$get_filter_count(), 4L)
+  })
+
+testthat::test_that(
+  "FilteredData$get_active_filtersproperly tallies active filter states for MAE objects",
+  code = {
+    datasets <- FilteredData$new(
+      list(
+        mae = list(dataset = miniACC)
+      ),
+      join_keys = NULL
+    )
+    fs <- list(
+      mae = list(
+        subjects = list(
+          years_to_birth = list(selected = c(30, 50), keep_na = TRUE, keep_inf = FALSE),
+          vital_status = list(selected = "1", keep_na = FALSE),
+          gender = list(selected = "female", keep_na = TRUE)
+        ),
+        RPPAArray = list(
+          subset = list(ARRAY_TYPE = list(selected = "", keep_na = TRUE))
+        )
+      )
+    )
+    testthat::expect_equal(datasets$.__enclos_env__$private$get_filter_count(), 0L)
+    datasets$set_filter_state(state = fs)
+    testthat::expect_equal(datasets$.__enclos_env__$private$get_filter_count(), 4L)
+  })
