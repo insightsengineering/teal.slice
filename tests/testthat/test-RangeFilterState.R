b48689725437f57ae10ff8ad0485a9811399922e
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-02-08 16:52:00 +0100

    adjust unit tests

--- tests/testthat/test-RangeFilterState.R
+++ tests/testthat/test-RangeFilterState.R
@@ -4,8 +4,8 @@ testthat::test_that("The constructor accepts numerical values", {
 
 testthat::test_that("The constructor accepts infinite values but not infinite only", {
   testthat::expect_no_error(RangeFilterState$new(c(1, Inf, -Inf), varname = "test"))
-  testthat::expect_error(RangeFilterState$new(Inf, varname = "test"), "any\\(is.finite\\(x")
-  testthat::expect_error(RangeFilterState$new(c(Inf, NA), varname = "test"), "any\\(is.finite\\(x")
+  testthat::expect_error(RangeFilterState$new(Inf, varname = "test"), "\"x\" contains no finite values")
+  testthat::expect_error(RangeFilterState$new(c(Inf, NA), varname = "test"), "\"x\" contains no finite values")
 })
 
 testthat::test_that("get_selected returns range computed on a vector containing c(1, Inf, -Inf, NA)", {
@@ -31,38 +31,19 @@ testthat::test_that("get_call returns a condition TRUE for all values passed to 
   testthat::expect_true(all(eval(shiny::isolate(filter_state$get_call()))))
 })
 
-testthat::test_that("set_selected throws an error if selecting completely outside of the possible range", {
-  filter_state <- RangeFilterState$new(7, varname = "test")
-  testthat::expect_error(
-    suppressWarnings(filter_state$set_selected(c(1, 3))),
-    regexp = "the upper bound of the range lower than the lower bound"
-  )
-})
+# testthat::test_that("set_selected warns when the selected range intersects the possible range
+#   but is not fully included in it", {
+#   filter_state <- RangeFilterState$new(7, varname = "test")
+#   testthat::expect_warning(filter_state$set_selected(c(1, 7)), "outside of the possible range")
+#   testthat::expect_warning(filter_state$set_selected(c(7, 13)), "outside of the possible range")
+#   testthat::expect_warning(filter_state$set_selected(c(1, 13)), "outside of the possible range")
+# })
 
-testthat::test_that("set_selected warns when the selected range intersects the possible range
-  but is not fully included in it", {
-  filter_state <- RangeFilterState$new(7, varname = "test")
-  testthat::expect_warning(filter_state$set_selected(c(1, 7)), "outside of the possible range")
-  testthat::expect_warning(filter_state$set_selected(c(7, 13)), "outside of the possible range")
-  testthat::expect_warning(filter_state$set_selected(c(1, 13)), "outside of the possible range")
-})
-
-testthat::test_that("set_selected defaults to the lower and the upper bound of the possible range
-  if the passed values exceed the possible range", {
-  filter_state <- RangeFilterState$new(c(7, 8), varname = "test")
-  suppressWarnings(filter_state$set_selected(c(1, 7)))
-  testthat::expect_equal(shiny::isolate(filter_state$get_selected()), c(7, 7))
-  suppressWarnings(filter_state$set_selected(c(7, 13)))
-  testthat::expect_equal(shiny::isolate(filter_state$get_selected()), c(7, 8))
-  suppressWarnings(filter_state$set_selected(c(1, 13)))
-  testthat::expect_equal(shiny::isolate(filter_state$get_selected()), c(7, 8))
-})
-
-testthat::test_that("set_selected throws when the passed values are not coercible to numeric", {
+testthat::test_that("set_selected raises error when the passed values are not coercible to numeric", {
   filter_state <- RangeFilterState$new(7, varname = "test")
   testthat::expect_error(
     filter_state$set_selected(c(print)),
-    "The array of set values must contain values coercible to numeric."
+    "Values to set must be an atomic vector."
   )
 })
 
