ae03989840fb25c3ac625d4d17ce43f29a2b18ce
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2023-02-16 10:25:02 +0100

    tidyup css

--- inst/js/count-bar-labels.js
+++ inst/js/count-bar-labels.js
@@ -1,9 +1,8 @@
 Shiny.addCustomMessageHandler("updateCountBar",
   function(message) {
-    /* update bar values and size*/
     let bar1 = document.getElementById(message.id + "-count_bar_filtered");
     let bar2 = document.getElementById(message.id + "-count_bar_unfiltered");
-
+    
     bar1.style.width = message.countnow / message.counttotal * 100 + "%";
     bar2.style.width = (message.countmax - message.countnow) / message.counttotal * 100 + "%";
     bar1.textContent = message.label;
