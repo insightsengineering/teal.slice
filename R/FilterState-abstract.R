f907256977c32c2cc0cb986f867432ed19cbf2b1
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-03-02 10:59:37 +0100

    fix documentation

--- R/FilterState-abstract.R
+++ R/FilterState-abstract.R
@@ -24,6 +24,14 @@ FilterState <- R6::R6Class( # nolint
     #'   label of the variable (optional).
     #' @param dataname (`character(1)`)\cr
     #'   name of dataset where `x` is taken from.
+    #' @param extract_type (`character(0)`, `character(1)`)\cr
+    #'   whether condition calls should be prefixed by dataname. Possible values:
+    #'   \itemize{
+    #'   \item{`character(0)` (default)}{ `varname` in the condition call will not be prefixed}
+    #'   \item{`"list"`}{ `varname` in the condition call will be returned as `<dataname>$<varname>`}
+    #'   \item{`"matrix"`}{ `varname` in the condition call will be returned as `<dataname>[, <varname>]`}
+    #'   }
+    #'
     #' @return self invisibly
     #'
     initialize = function(x,
