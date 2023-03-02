cef4e26f9e5cc63fb3dfbf6206110b85fc5c8769
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-03-02 11:38:37 +0100

    amend unit tests

--- tests/testthat/test-LogicalFilterState.R
+++ tests/testthat/test-LogicalFilterState.R
@@ -1,15 +1,15 @@
 logs <- as.logical(c(rbinom(10, 1, 0.5), NA))
 
 testthat::test_that("The constructor accepts logical values", {
-  testthat::expect_no_error(LogicalFilterState$new(c(TRUE), varname = "test"))
+  testthat::expect_no_error(LogicalFilterState$new(c(TRUE), x_reactive = reactive(NULL), varname = "test"))
 })
 
 testthat::test_that("The constructor accepts NA values", {
-  testthat::expect_no_error(LogicalFilterState$new(c(TRUE, NA), varname = "test"))
+  testthat::expect_no_error(LogicalFilterState$new(c(TRUE, NA), x_reactive = reactive(NULL), varname = "test"))
 })
 
 testthat::test_that("get_call returns FALSE values from data passed to selector", {
-  filter_state <- LogicalFilterState$new(logs, varname = "logs")
+  filter_state <- LogicalFilterState$new(logs, x_reactive = reactive(NULL), varname = "logs")
   expect_identical(
     eval(shiny::isolate(filter_state$get_call())),
     !logs
@@ -17,21 +17,21 @@ testthat::test_that("get_call returns FALSE values from data passed to selector"
 })
 
 testthat::test_that("set_selected accepts a logical of length 1", {
-  filter_state <- LogicalFilterState$new(logs, varname = "logs")
+  filter_state <- LogicalFilterState$new(logs, x_reactive = reactive(NULL), varname = "logs")
   testthat::expect_no_error(filter_state$set_selected(TRUE))
   testthat::expect_no_error(filter_state$set_selected(FALSE))
   testthat::expect_error(filter_state$set_selected(c(TRUE, TRUE)), "should be a logical scalar")
 })
 
 testthat::test_that("set_selected accepts a non-logical coercible to logical of length 1", {
-  filter_state <- LogicalFilterState$new(logs, varname = "logs")
+  filter_state <- LogicalFilterState$new(logs, x_reactive = reactive(NULL), varname = "logs")
   testthat::expect_no_error(filter_state$set_selected("TRUE"))
   testthat::expect_no_error(filter_state$set_selected("FALSE"))
   testthat::expect_error(filter_state$set_selected(c("TRUE", "TRUE")), "should be a logical scalar")
 })
 
 testthat::test_that("get_call returns appropriate call depending on selection state", {
-  filter_state <- LogicalFilterState$new(logs, varname = "logs")
+  filter_state <- LogicalFilterState$new(logs, x_reactive = reactive(NULL), varname = "logs")
   expect_identical(
     shiny::isolate(filter_state$get_call()),
     quote(!logs)
@@ -49,20 +49,20 @@ testthat::test_that("get_call returns appropriate call depending on selection st
 })
 
 testthat::test_that("set_state needs a named list with selected and keep_na elements", {
-  filter_state <- LogicalFilterState$new(x = c(TRUE, FALSE, NA), varname = "test")
+  filter_state <- LogicalFilterState$new(x = c(TRUE, FALSE, NA), x_reactive = reactive(NULL), varname = "test")
   testthat::expect_no_error(filter_state$set_state(list(selected = FALSE, keep_na = TRUE)))
   testthat::expect_error(filter_state$set_state(list(selected = TRUE, unknown = TRUE)), "all\\(names\\(state\\)")
 })
 
 testthat::test_that("set_state sets values of selected and keep_na as provided in the list", {
-  filter_state <- LogicalFilterState$new(x = c(TRUE, FALSE, NA), varname = "test")
+  filter_state <- LogicalFilterState$new(x = c(TRUE, FALSE, NA), x_reactive = reactive(NULL), varname = "test")
   filter_state$set_state(list(selected = FALSE, keep_na = TRUE))
   testthat::expect_identical(shiny::isolate(filter_state$get_selected()), FALSE)
   testthat::expect_true(shiny::isolate(filter_state$get_keep_na()))
 })
 
 testthat::test_that("set_state overwrites fields included in the input only", {
-  filter_state <- LogicalFilterState$new(x = c(TRUE, FALSE, NA), varname = "test")
+  filter_state <- LogicalFilterState$new(x = c(TRUE, FALSE, NA), x_reactive = reactive(NULL), varname = "test")
   filter_state$set_state(list(selected = FALSE, keep_na = TRUE))
   testthat::expect_no_error(filter_state$set_state(list(selected = TRUE)))
   testthat::expect_true(shiny::isolate(filter_state$get_selected()))
@@ -73,7 +73,8 @@ testthat::test_that(
   "LogicalFilterState$is_any_filtered works properly when NA is present in data",
   code = {
     filter_state <- teal.slice:::LogicalFilterState$new(
-      rep(c(TRUE, NA), 10),
+      x = rep(c(TRUE, NA), 10),
+      x_reactive = reactive(NULL),
       varname = "x",
       dataname = "data",
       extract_type = character(0)
@@ -96,7 +97,8 @@ testthat::test_that(
   "LogicalFilterState$is_any_filtered works properly when both TRUE and FALSE are present",
   code = {
     filter_state <- teal.slice:::LogicalFilterState$new(
-      rep(c(TRUE, FALSE, NA), 10),
+      x = rep(c(TRUE, FALSE, NA), 10),
+      x_reactive = reactive(NULL),
       varname = "x",
       dataname = "data",
       extract_type = character(0)
@@ -141,7 +143,8 @@ testthat::test_that(
   "LogicalFilterState$is_any_filtered works properly when only TRUE or FALSE is present",
   code = {
     filter_state <- teal.slice:::LogicalFilterState$new(
-      rep(c(TRUE, NA), 10),
+      x = rep(c(TRUE, NA), 10),
+      x_reactive = reactive(NULL),
       varname = "x",
       dataname = "data",
       extract_type = character(0)
@@ -171,7 +174,8 @@ testthat::test_that(
     )
 
     filter_state <- teal.slice:::LogicalFilterState$new(
-      rep(c(FALSE, NA), 10),
+      x = rep(c(FALSE, NA), 10),
+      x_reactive = reactive(NULL),
       varname = "x",
       dataname = "data",
       extract_type = character(0)
