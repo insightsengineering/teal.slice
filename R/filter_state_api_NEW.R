cc4c01514312c9545ab1f67704b501979a96bb09
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-03-07 10:53:08 +0100

    update arguments in filter_var

--- R/filter_state_api_NEW.R
+++ R/filter_state_api_NEW.R
@@ -5,20 +5,22 @@
 #'
 #' @param dataname `character(1)` name of data set
 #' @param varname `character(1)` name of variable
-#' @param choices vector specifying choices choices; type and size depends on variable type
-#' @param selected vector specifying initial choices; type and size depends on variable type
+#' @param choices vector specifying allowed choices;
+#'                possibly a subset of values in data;
+#'                type and size depends on variable type
+#' @param selected vector specifying selection;
+#'                 type and size depends on variable type
 #' @param varlabel `character(0-1)` optional variable label
-#' @param keep_na `logical(1)` logical flag specifying the initial value of `private$keep_na` field
-#' @param keep_inf `logical(0-1)` optional logical flag specifying the initial value of `private$keep_na` field
+#' @param keep_na `logical(0-1)` optional logical flag specifying whether to keep missing values
+#' @param keep_inf `logical(0-1)` optional logical flag specifying whether to keep infinite values
 #' @param fixed `logical(1)` logical flag specifying whether to fix this filter state (i.e. forbid setting state)
-#' @param ... optional additional information given as `name:value` pairs, to be stored in `private$extras`
 #'
 #' @return
 #' Object of class `teal_slice`, which is a named list.
 #'
 #' @examples
-#' filter_one <- filter_var("dataname1", "varname1", letters, "b", "characters", FALSE, FALSE, extra1 = "extraone1", extra2 = "extraone2")
-#' filter_two <- filter_var("dataname1", "varname2", 1:10, 2, "integers", TRUE, FALSE, extra1 = "extratwo1", extra2 = "extratwo2")
+#' filter_one <- filter_var("dataname1", "varname1", letters, "b", "characters", FALSE)
+#' filter_two <- filter_var("dataname1", "varname2", 1:10, 2, "integers", TRUE, FALSE)
 #' filter_three <- filter_var("dataname1", "varname3", 1:10/10, 0.2, "doubles", TRUE, FALSE)
 #'
 #' @export
@@ -27,31 +29,31 @@
 filter_var <- function(
     dataname,
     varname,
-    choices,
-    selected,
+    choices = NULL,
+    selected = NULL,
     varlabel = NULL,
-    keep_na = FALSE,
+    keep_na = NULL,
     keep_inf = NULL,
-    fixed = FALSE,
-    ...
+    fixed = FALSE
 ) {
   checkmate::assert_string(dataname)
   checkmate::assert_string(varname)
-  # assertion on choices dependent on variable type
-  # assertion on selected dependent on variable type
+  checkmate::assert_atomic(choices)
+  checkmate::assert_atomic(selected)
+  checkmate::assert_subset(selected, choices)
   checkmate::assert_string(varlabel, null.ok = TRUE)
-  checkmate::assert_flag(keep_na)
+  checkmate::assert_flag(keep_na, null.ok = TRUE)
   checkmate::assert_flag(keep_inf, null.ok = TRUE)
   checkmate::assert_flag(fixed)
 
   ans <- as.list(match.call())[-1]
-  extras <- ans[!is.element(names(ans), formalArgs(filter_var))] # extras <- list(...)
+  ans <- lapply(ans, eval)
   class(ans) <- "teal_slice"
   ans
 }
 
-filter_one <- filter_var("dataname1", "varname1", letters, "b", "characters", FALSE, FALSE, extra1 = "extraone1", extra2 = "extraone2")
-filter_two <- filter_var("dataname1", "varname2", 1:10, 2, "integers", TRUE, FALSE, extra1 = "extratwo1", extra2 = "extratwo2")
+filter_one <- filter_var("dataname1", "varname1", letters, "b", "characters", FALSE, FALSE)
+filter_two <- filter_var("dataname1", "varname2", 1:10, 2, "integers", TRUE, FALSE)
 filter_three <- filter_var("dataname1", "varname3", 1:10/10, 0.2, "doubles", TRUE, FALSE)
 
 
@@ -62,6 +64,7 @@ filter_three <- filter_var("dataname1", "varname3", 1:10/10, 0.2, "doubles", TRU
 #' @param ... any number of `teal_slice` objects
 #' @filterable `named list` of `character` vectors, specifying which variables
 #'              (list elements) can be filtered in which data sets (list names)
+#' @count_type `character(1)` string specifiying how observations are tallied by these filter states
 #'
 #' @return
 #' Object of class `teal_slices`, which is an unnamed list of `teal_slice` objects.
@@ -72,11 +75,23 @@ filter_three <- filter_var("dataname1", "varname3", 1:10/10, 0.2, "doubles", TRU
 #' @export
 #' @rdname new_api
 #'
-filter_settings <- function(..., filterable = list()) {
+filter_settings <- function(
+    ...,
+    filterable = list(),
+    count_type = c("none", "all", "hierarchical")
+) {
   checkmate::assert_list(filterable, names = "named", types = "character")
   args <- list(...)
   checkmate::assert_list(args, types = "teal_slice", any.missing = FALSE)
   class(args) <- "teal_slices"
+  count_type <- match_arg(count_type)
+  args <- append(
+    args,
+    list(
+      filterable = filterable,
+      count_type = count_type
+    )
+  )
   args
 }
 all_filters <- filter_settings(filter_one, filter_two, filter_three)
