261ab7665744d2545e4f30a63dd7a15fd4dc5059
Blazewicz <blazewim@emea.roche.com>
2023-03-31 14:45:34 +0200

    format bugfix

--- R/FilterState.R
+++ R/FilterState.R
@@ -183,7 +183,7 @@ FilterState <- R6::R6Class( # nolint
     #' @param ... additional arguments to this method
     #'
     print = function(...) {
-      cat(shiny::isolate(private$format()), "\n")
+      cat(shiny::isolate(self$format()), "\n")
     },
 
     #' @description
