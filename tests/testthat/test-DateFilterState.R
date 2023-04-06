9f502f94f0bd156c21b36bfd5900456ce0e6f57d
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-04-06 11:59:27 +0200

    update tests for DateFilterState

--- tests/testthat/test-DateFilterState.R
+++ tests/testthat/test-DateFilterState.R
@@ -28,7 +28,7 @@ testthat::test_that("constructor raises error when selection is not Date", {
 })
 
 # set_state ----
-testthat::test_that("set_state: selected accepts an array of two Date objects", {
+testthat::test_that("set_state: selected accepts vector of two Date objects", {
   filter_state <- DateFilterState$new(dates, dataname = "data", varname = "variable")
   testthat::expect_no_error(
     filter_state$set_state(filter_var(selected = dates[c(1, 10)], dataname = "data", varname = "variable"))
@@ -37,9 +37,13 @@ testthat::test_that("set_state: selected accepts an array of two Date objects", 
     filter_state$set_state(filter_var(selected = dates[1], dataname = "data", varname = "variable")),
     "The array of set values must have length two."
   )
+  testthat::expect_error(
+    filter_state$set_state(filter_var(selected = c(1, 10), dataname = "data", varname = "variable")),
+    "The array of set values must contain values coercible to Date"
+  )
 })
 
-testthat::test_that("set_state: selected warns when selection is not within the possible range", {
+testthat::test_that("set_state raises warning when selection is not within the possible range", {
   filter_state <- DateFilterState$new(dates, dataname = "data", varname = "variable")
 
   testthat::expect_warning(
@@ -62,7 +66,7 @@ testthat::test_that("set_state: selected warns when selection is not within the 
   )
 })
 
-testthat::test_that("set_state: selected limits the selected range to lower and upper bound of possible range", {
+testthat::test_that("set_state: selected range is limited to lower and upper bound of possible range", {
   filter_state <- DateFilterState$new(dates, dataname = "data", varname = "variable")
   suppressWarnings(filter_state$set_state(
     filter_var(dataname = "data", varname = "variable", selected = c(dates[1] - 1, dates[10]))
@@ -99,47 +103,6 @@ testthat::test_that("get_call returns call with limits imposed by constructor an
     quote(variable >= as.Date("2000-01-03") & variable <= as.Date("2000-01-04"))
   )
 })
-
-testthat::test_that(
-  "DateFilterState$is_any_filtered works properly when NA is present in data",
-  code = {
-    filter_state <- teal.slice:::DateFilterState$new(
-      x = c(dates, NA),
-      x_reactive = reactive(NULL),
-      varname = "x",
-      dataname = "data",
-      extract_type = character(0)
-    )
-
-    shiny::isolate(filter_state$set_state(filter_var(
-      varname = "x", dataname = "data", keep_na = FALSE, selected = c(dates[1], dates[10]))
-    ))
-    testthat::expect_true(
-      shiny::isolate(filter_state$is_any_filtered())
-    )
-
-    shiny::isolate(filter_state$set_state(filter_var(
-      varname = "x", dataname = "data", keep_na = TRUE, selected = c(dates[1], dates[10]))
-    ))
-    testthat::expect_false(
-      shiny::isolate(filter_state$is_any_filtered())
-    )
-
-    shiny::isolate(filter_state$set_state(filter_var(
-      varname = "x", dataname = "data", keep_na = TRUE, selected = c(dates[2], dates[10]))
-    ))
-    testthat::expect_true(
-      shiny::isolate(filter_state$is_any_filtered())
-    )
-
-    shiny::isolate(filter_state$set_state(filter_var(
-      varname = "x", dataname = "data", keep_na = TRUE, selected = c(dates[1], dates[9]))
-    ))
-    testthat::expect_true(
-      shiny::isolate(filter_state$is_any_filtered())
-    )
-  }
-)
 
 # format ----
 testthat::test_that("format returns a properly formatted string representation", {
@@ -186,6 +149,45 @@ testthat::test_that("format prepends spaces to every line of the returned string
 })
 
 # is_any_filtered
+# is_any_filtered ----
+testthat::test_that("is_any_filtered works properly when NA is present in data", {
+  filter_state <- teal.slice:::DateFilterState$new(
+    x = c(dates, NA),
+    x_reactive = reactive(NULL),
+    varname = "x",
+    dataname = "data",
+    extract_type = character(0)
+  )
+
+  shiny::isolate(filter_state$set_state(filter_var(
+    varname = "x", dataname = "data", keep_na = FALSE, selected = c(dates[1], dates[10]))
+  ))
+  testthat::expect_true(
+    shiny::isolate(filter_state$is_any_filtered())
+  )
+
+  shiny::isolate(filter_state$set_state(filter_var(
+    varname = "x", dataname = "data", keep_na = TRUE, selected = c(dates[1], dates[10]))
+  ))
+  testthat::expect_false(
+    shiny::isolate(filter_state$is_any_filtered())
+  )
+
+  shiny::isolate(filter_state$set_state(filter_var(
+    varname = "x", dataname = "data", keep_na = TRUE, selected = c(dates[2], dates[10]))
+  ))
+  testthat::expect_true(
+    shiny::isolate(filter_state$is_any_filtered())
+  )
+
+  shiny::isolate(filter_state$set_state(filter_var(
+    varname = "x", dataname = "data", keep_na = TRUE, selected = c(dates[1], dates[9]))
+  ))
+  testthat::expect_true(
+    shiny::isolate(filter_state$is_any_filtered())
+  )
+})
+
 testthat::test_that("is_any_filtered returns TRUE when enabled and FLASE when disabled", {
   testfs <- R6::R6Class(
     classname = "testfs",
@@ -195,9 +197,7 @@ testthat::test_that("is_any_filtered returns TRUE when enabled and FLASE when di
       enable = function() private$enable()
     )
   )
-  date_seq <- seq(Sys.Date() - 2, Sys.Date(), 1)
-  fs <- testfs$new(date_seq, dataname = "data", varname = "x")
-  fs$set_state(filter_var(dataname = "data", varname = "x", selected = date_seq[1:2], keep_na = TRUE))
+  fs <- testfs$new(dates, dataname = "data", varname = "variable", selected = dates[1:2])
   testthat::expect_true(shiny::isolate(fs$is_any_filtered()))
   shiny::isolate(fs$disable())
   testthat::expect_false(shiny::isolate(fs$is_any_filtered()))
@@ -214,10 +214,9 @@ testthat::test_that("is_any_filtered reacts to choices", {
       enable = function() private$enable()
     )
   )
-  date_seq <- seq(Sys.Date() - 4, Sys.Date(), 1)
-  fs <- testfs$new(date_seq, dataname = "data", varname = "x", choices = date_seq[c(1, 2)])
-  testthat::expect_true(isolate(fs$is_any_filtered()))
-  fs <- testfs$new(date_seq, dataname = "data", varname = "x", choices = date_seq[c(1, 5)])
-  testthat::expect_false(isolate(fs$is_any_filtered()))
+  fs <- testfs$new(dates, dataname = "data", varname = "variable")
+  testthat::expect_false(shiny::isolate(fs$is_any_filtered()))
+  fs <- testfs$new(dates, dataname = "data", varname = "x", choices = dates[c(1, 2)])
+  testthat::expect_true(shiny::isolate(fs$is_any_filtered()))
 })
 
