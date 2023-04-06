5a816c6ffc1e2c77d4f6f3a4e0161e895b3dfc2f
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-04-06 15:20:57 +0200

    update tests for DatetimeFilterState

--- tests/testthat/test-DatetimeFilterState.R
+++ tests/testthat/test-DatetimeFilterState.R
@@ -1,34 +1,110 @@
 posixct <- as.POSIXct("2000-01-01 12:00:00", tz = "GMT") + 0:9
 posixlt <- as.POSIXlt(posixct)
 
+# initialize ----
 testthat::test_that("constructor accepts a POSIXct or POSIXlt object", {
   testthat::expect_no_error(
-    DatetimeFilterState$new(posixct, x_reactive = reactive(NULL), varname = "variable", dataname = "data")
+    DatetimeFilterState$new(posixct, dataname = "data", varname = "variable")
   )
   testthat::expect_no_error(
-    DatetimeFilterState$new(posixlt, x_reactive = reactive(NULL), varname = "variable", dataname = "data")
+    DatetimeFilterState$new(posixlt, dataname = "data", varname = "variable")
+  )
+  testthat::expect_error(
+    DatetimeFilterState$new(as.Date(posixct), dataname = "data", varname = "variable"),
+    "Assertion on 'x' failed"
   )
 })
 
+testthat::test_that("constructor raises warning when selected is not sorted", {
+  testthat::expect_warning(
+    DatetimeFilterState$new(
+      posixct, dataname = "data", varname = "variable", selected = posixct[c(10, 1)]
+    ),
+    "Start date '2000-01-01 12:00:09' is set after"
+  )
+})
+
+testthat::test_that("constructor raises error when selection is not Date", {
+  testthat::expect_error(
+    DatetimeFilterState$new(posixct, dataname = "data", varname = "variable", selected = c("a", "b")),
+    "The array of set values must contain values coercible to POSIX"
+  )
+})
+
+
+# set_state ----
+testthat::test_that("set_state: selected accepts vector of two POSIXct objects or coercible", {
+  filter_state <- DatetimeFilterState$new(posixct, dataname = "data", varname = "variable")
+
+  testthat::expect_no_error(
+    filter_state$set_state(filter_var(dataname = "data", varname = "variable", selected = posixct[1:2]))
+  )
+  testthat::expect_error(
+    filter_state$set_state(filter_var(dataname = "data", varname = "variable", selected = posixct[1])),
+    "The array of set values must have length two."
+  )
+  testthat::expect_error(
+    filter_state$set_state(filter_var(dataname = "data", varname = "variable", selected = 1:2)),
+    "The array of set values must contain values coercible to POSIX"
+  )
+})
+
+testthat::test_that("set_state: selected raises warning when selection not fully included in range", {
+  filter_state <- DatetimeFilterState$new(posixct, dataname = "data", varname = "posixct")
+
+  testthat::expect_warning(
+    filter_state$set_state(
+      filter_var(dataname = "data", varname = "posixct", selected = c(posixct[1] - 1, posixct[10]))
+    ),
+    "outside of the range"
+  )
+  testthat::expect_warning(
+    filter_state$set_state(
+      filter_var(dataname = "data", varname = "posixct", selected = c(posixct[1], posixct[10] + 1))
+    ),
+    "outside of the range"
+  )
+  testthat::expect_warning(
+    filter_state$set_state(
+      filter_var(dataname = "data", varname = "posixct", selected = c(posixct[1] - 1, posixct[10] + 1))
+    ),
+    "outside of the range"
+  )
+})
+
+testthat::test_that("set_state: selected range is limited to lower and upper bound of possible range", {
+  filter_state <- DatetimeFilterState$new(posixct, dataname = "data", varname = "posixct")
+
+  suppressWarnings(filter_state$set_state(
+    filter_var(dataname = "data", varname = "posixct", selected = c(posixct[1] - 1, posixct[10]))
+  ))
+  testthat::expect_equal(shiny::isolate(filter_state$get_state()$selected), c(posixct[1], posixct[10]))
+
+  suppressWarnings(filter_state$set_state(
+    filter_var(dataname = "data", varname = "posixct", selected = c(posixct[1], posixct[10] + 1))
+  ))
+  testthat::expect_equal(shiny::isolate(filter_state$get_state()$selected), c(posixct[1], posixct[10]))
+
+  suppressWarnings(filter_state$set_state(
+    filter_var(dataname = "data", varname = "posixct", selected = c(posixct[1] - 1, posixct[10] + 1))
+  ))
+  testthat::expect_equal(shiny::isolate(filter_state$get_state()$selected), c(posixct[1], posixct[10]))
+})
+
+
+# get_call ----
 testthat::test_that("get_call returns call that encompasses all values passed to constructor", {
-  filter_state <- DatetimeFilterState$new(posixct, x_reactive = reactive(NULL), varname = "variable", dataname = "data")
+  filter_state <- DatetimeFilterState$new(posixct, dataname = "data", varname = "variable")
   testthat::expect_identical(
     shiny::isolate(filter_state$get_call()),
     quote(variable >= as.POSIXct("2000-01-01 12:00:00", tz = "GMT") & variable <
-      as.POSIXct("2000-01-01 12:00:10", tz = "GMT"))
-  )
-})
-
-testthat::test_that("set_state: selected accepts an array of two POSIXct objects", {
-  filter_state <- DatetimeFilterState$new(posixct, x_reactive = reactive(NULL), varname = "variable", dataname = "data")
-  testthat::expect_no_error(
-    filter_state$set_state(filter_var(selected = posixct[1:2], varname = "variable", dataname = "data"))
+            as.POSIXct("2000-01-01 12:00:10", tz = "GMT"))
   )
 })
 
 testthat::test_that("get_call returns a condition true for the object in the selected range", {
-  filter_state <- DatetimeFilterState$new(posixct, x_reactive = reactive(NULL), varname = "variable", dataname = "data")
-  filter_state$set_state(filter_var(selected = posixct[2:3], varname = "variable", dataname = "data"))
+  filter_state <- DatetimeFilterState$new(posixct, dataname = "data", varname = "variable")
+  filter_state$set_state(filter_var(dataname = "data", varname = "variable", selected = posixct[2:3]))
   variable <- posixct[1:4]
   testthat::expect_equal(
     eval(shiny::isolate(filter_state$get_call())),
@@ -43,185 +119,75 @@ testthat::test_that("get_call returns a condition true for the object in the sel
   )
 })
 
-testthat::test_that("get_call returns a condition evaluating to TRUE for NA values after set_keep_na(TRUE)", {
+testthat::test_that("get_call returns a condition evaluating to TRUE for NA values is keep_na is TRUE", {
   variable <- c(posixct, NA)
   filter_state <- DatetimeFilterState$new(
-    variable, x_reactive = reactive(NULL), varname = "variable", dataname = "data")
+    variable, dataname = "data", varname = "variable")
   testthat::expect_identical(eval(shiny::isolate(filter_state$get_call()))[11], NA)
-  filter_state$set_state(filter_var(keep_na = TRUE, varname = "variable", dataname = "data"))
+  filter_state$set_state(filter_var(dataname = "data", varname = "variable", keep_na = TRUE))
   testthat::expect_identical(eval(shiny::isolate(filter_state$get_call()))[11], TRUE)
 })
 
 
-testthat::test_that("DatetimeFilterState echoes the timezone of the ISO object passed to the constructor", {
-  objects <- ISOdate(2021, 8, 25, tz = "Australia/Brisbane")
+testthat::test_that("get_call preserves timezone of ISO object passed to constructor", {
+  variable <- ISOdate(2021, 8, 25, tz = "Australia/Brisbane")
   filter_state <- DatetimeFilterState$new(
-    objects, x_reactive = reactive(NULL), varname = "objects", dataname = "data")
+    variable, dataname = "data", varname = "variable")
   testthat::expect_equal(
     shiny::isolate(filter_state$get_call()),
     quote(
-      objects >= as.POSIXct("2021-08-25 12:00:00", tz = "Australia/Brisbane") &
-        objects < as.POSIXct("2021-08-25 12:00:01", tz = "Australia/Brisbane")
+      variable >= as.POSIXct("2021-08-25 12:00:00", tz = "Australia/Brisbane") &
+        variable < as.POSIXct("2021-08-25 12:00:01", tz = "Australia/Brisbane")
     )
   )
 })
 
-testthat::test_that(
-  "set_state: selected warns when the selected range intersects the range but is not fully included in it", {
-  objects <- as.POSIXct(c(2, 3), origin = "1900/01/01 00:00:00")
-  filter_state <- DatetimeFilterState$new(objects, varname = "objects", dataname = "data")
-  testthat::expect_warning(
-    filter_state$set_state(
-      filter_var(varname = "objects", dataname = "data", selected = c(objects[1] - 1, objects[1]))
-    ),
-    "outside of the range"
-  )
-  testthat::expect_warning(
-    filter_state$set_state(
-      filter_var(varname = "objects", dataname = "data", selected = c(objects[2], objects[2] + 1))
-    ),
-    "outside of the range"
-  )
-  testthat::expect_warning(
-    filter_state$set_state(
-      filter_var(varname = "objects", dataname = "data", selected = c(objects[1] - 1, objects[2] + 1))
-    ),
-    "outside of the range"
-  )
-})
 
-testthat::test_that("set_state: selected throws when the selected range is completely outside of the possible range", {
-  objects <- as.POSIXct(c(2, 3), origin = "1900/01/01 00:00:00")
-  filter_state <- DatetimeFilterState$new(objects, x_reactive = reactive(NULL), varname = "objects", dataname = "data")
-  testthat::expect_warning(
-    filter_state$set_state(
-      filter_var(selected = c(objects[2] + 1, objects[2] + 2), varname = "objects", dataname = "data")
-    ),
-    "is outside of the range"
-  )
-})
-
-testthat::test_that(
-  "set_state: selected limits the selected range to the lower and the upper bound of the possible range", {
-  objects <- as.POSIXct(c(2, 3), origin = "1900/01/01 00:00:00")
-  filter_state <- DatetimeFilterState$new(objects, x_reactive = reactive(NULL), varname = "objects", dataname = "data")
-  suppressWarnings(filter_state$set_state(
-    filter_var(varname = "objects", dataname = "data", selected = c(objects[1] - 1, objects[1]))
-  ))
-
-  testthat::expect_equal(shiny::isolate(filter_state$get_state()$selected), c(objects[1], objects[1]))
-  suppressWarnings(filter_state$set_state(
-    filter_var(varname = "objects", dataname = "data", selected = c(objects[2], objects[2] + 1))
-  ))
-  testthat::expect_equal(shiny::isolate(filter_state$get_state()$selected), c(objects[2], objects[2]))
-
-  suppressWarnings(filter_state$set_state(
-    filter_var(varname = "objects", dataname = "data", selected = c(objects[1] - 1, objects[2] + 1))
-  ))
-  testthat::expect_equal(shiny::isolate(filter_state$get_state()$selected), c(objects[1], objects[2]))
-})
-
-testthat::test_that("set_state: selected throws when the value type cannot be interpreted as POSIX", {
-  objects <- as.POSIXct(c(1, 2, 3), origin = "1900/01/01 00:00:00")
-  filter_state <- DatetimeFilterState$new(objects, x_reactive = reactive(NULL), varname = "objects", dataname = "data")
-  testthat::expect_error(
-    filter_state$set_state(filter_var(selected = c("a", "b"), varname = "objects", dataname = "data")),
-    "The array of set values must contain values coercible to POSIX."
-  )
-})
-
-testthat::test_that("set_state sets values of selected and keep_na as provided in the list", {
-  objects <- as.POSIXct(c(1:4), origin = "1900/01/01 00:00:00")
-  filter_state <- DatetimeFilterState$new(objects, varname = "test", dataname = "data")
-  filter_state$set_state(
-    filter_var(selected = c(objects[2], objects[3]), keep_na = TRUE, varname = "test", dataname = "data"))
-  testthat::expect_identical(shiny::isolate(filter_state$get_state()$selected), c(objects[2], objects[3]))
-  testthat::expect_true(shiny::isolate(filter_state$get_state()$keep_na))
-})
-
-testthat::test_that("set_state overwrites fields included in the input only", {
-  objects <- as.POSIXct(c(1:5), origin = "1900/01/01 00:00:00")
-  filter_state <- DatetimeFilterState$new(objects, x_reactive = reactive(NULL), varname = "test", dataname = "data")
-  filter_state$set_state(
-    filter_var(selected = c(objects[2], objects[3]), keep_na = TRUE, varname = "test", dataname = "data"))
-  testthat::expect_no_error(filter_state$set_state(
-    filter_var(selected = c(objects[3], objects[4]), varname = "test", dataname = "data")))
-  testthat::expect_identical(shiny::isolate(filter_state$get_state()$selected), c(objects[3], objects[4]))
-  testthat::expect_true(shiny::isolate(filter_state$get_state()$keep_na))
-})
-
-testthat::test_that(
-  "DatetimeFilterState$is_any_filtered works properly when NA is present in data",
+testthat::test_that("is_any_filtered works properly when NA is present in data",
   code = {
     filter_state <- teal.slice:::DatetimeFilterState$new(
       x = c(posixct, NA),
       x_reactive = reactive(NULL),
-      varname = "x",
+      varname = "variable",
       dataname = "data",
       extract_type = character(0)
     )
 
-    shiny::isolate(filter_state$set_state(filter_var(keep_na = FALSE, varname = "x", dataname = "data")))
+    shiny::isolate(filter_state$set_state(filter_var(keep_na = FALSE, varname = "variable", dataname = "data")))
     testthat::expect_true(
       shiny::isolate(filter_state$is_any_filtered())
     )
 
-    shiny::isolate(filter_state$set_state(filter_var(keep_na = TRUE, varname = "x", dataname = "data")))
+    shiny::isolate(filter_state$set_state(filter_var(keep_na = TRUE, varname = "variable", dataname = "data")))
     testthat::expect_false(
       shiny::isolate(filter_state$is_any_filtered())
     )
   }
 )
 
-# Format
-testthat::test_that("$format() is a FilterStates's method that accepts indent", {
-  object <- as.POSIXct(8, origin = "1900/01/01 00:00:00", tz = "GMT")
-  filter_state <- DatetimeFilterState$new(object, x_reactive = reactive(NULL), varname = "test", dataname = "data")
+# format ----
+testthat::test_that("format accepts numeric as indent", {
+  filter_state <- DatetimeFilterState$new(posixct, dataname = "data", varname = "variable")
+  testthat::expect_no_error(shiny::isolate(filter_state$format(indent = 0L)))
   testthat::expect_no_error(shiny::isolate(filter_state$format(indent = 0)))
+  testthat::expect_error(shiny::isolate(filter_state$format(indent = "0")), "Assertion on 'indent' failed")
 })
 
-testthat::test_that("$format() asserts that indent is numeric", {
-  object <- as.POSIXct(8, origin = "1900/01/01 00:00:00", tz = "GMT")
-  filter_state <- DatetimeFilterState$new(object, x_reactive = reactive(NULL), varname = "test", dataname = "data")
-  testthat::expect_error(
-    filter_state$format(indent = "wrong type"),
-    regexp = "Assertion on 'indent' failed: Must be of type 'number'"
-  )
-})
-
-testthat::test_that("$format() returns a string representation the FilterState object", {
-  # # This test is skipped on versions lower than 4.1.0 because on lower versions
-  # # the `c` function drops the time zone attribute silently from the POSIXct objects,
-  # # the state set in `filter_state` is incorrect and the test fails.
-  testthat::skip_if(
-    utils::compareVersion(sprintf("%s.%s", version["major"], version["minor"]), "4.1.0") < 0,
-    message = "Skipped on R versions lower than 4.1.0"
-  )
-  object <- as.POSIXct(8, origin = "1900/01/01 00:00:00", tz = "GMT")
-  filter_state <- DatetimeFilterState$new(object, x_reactive = reactive(NULL), varname = "test", dataname = "data")
-  filter_state$set_state(filter_var(selected = c(object, object), varname = "test", dataname = "data"))
+testthat::test_that("format returns a properly formatted string representation", {
+  filter_state <- DatetimeFilterState$new(posixct, dataname = "data", varname = "variable")
   testthat::expect_equal(
     shiny::isolate(filter_state$format(indent = 0)),
     paste(
-      "Filtering on: test",
-      "  Selected range: 1900-01-01 00:00:08 - 1900-01-01 00:00:08",
+      "Filtering on: variable",
+      "  Selected range: 2000-01-01 12:00:00 - 2000-01-01 12:00:09",
       "  Include missing values: FALSE",
       sep = "\n"
     )
   )
 })
 
-testthat::test_that("$format() prepends spaces to every line of the returned string", {
-  # This test is skipped on versions lower than 4.1.0 because on lower versions
-  # the `c` function drops the time zone attribute silently from the POSIXct objects,
-  # the state set in `filter_state` is incorrect and the test fails.
-  # testthat::skip_if(
-  #   utils::compareVersion(sprintf("%s.%s", version["major"], version["minor"]), "4.1.0") < 0,
-  #   message = "Skipped on R versions lower than 4.1.0"
-  # )
-  object <- as.POSIXct(8, origin = "1900/01/01 00:00:00", tz = "GMT")
-  filter_state <- DatetimeFilterState$new(object, x_reactive = reactive(NULL), varname = "test", dataname = "data")
-  filter_state$set_state(filter_var(selected = c(object, object), varname = "test", dataname = "data"))
+testthat::test_that("format prepends spaces to every line of the returned string", {
+  filter_state <- DatetimeFilterState$new(posixct, varname = "test", dataname = "data")
   for (i in 1:3) {
     whitespace_indent <- paste0(rep(" ", i), collapse = "")
     testthat::expect_equal(
@@ -229,7 +195,7 @@ testthat::test_that("$format() prepends spaces to every line of the returned str
       sprintf(
         paste(
           "%sFiltering on: test",
-          "%1$s  Selected range: 1900-01-01 00:00:08 - 1900-01-01 00:00:08",
+          "%1$s  Selected range: 2000-01-01 12:00:00 - 2000-01-01 12:00:09",
           "%1$s  Include missing values: FALSE",
           sep = "\n"
         ),
@@ -239,9 +205,23 @@ testthat::test_that("$format() prepends spaces to every line of the returned str
   }
 })
 
+# is_any_filtered ----
+testthat::test_that("is_any_filtered works properly when NA is present in data", {
+  filter_state <- teal.slice:::DatetimeFilterState$new(c(posixct, NA), varname = "variable", dataname = "data")
+
+  shiny::isolate(filter_state$set_state(filter_var(varname = "variable", dataname = "data", keep_na = FALSE)))
+  testthat::expect_true(shiny::isolate(filter_state$is_any_filtered()))
+
+  shiny::isolate(filter_state$set_state(filter_var(varname = "variable", dataname = "data", keep_na = TRUE)))
+  testthat::expect_false(shiny::isolate(filter_state$is_any_filtered()))
+
+  shiny::isolate(filter_state$set_state(
+    filter_var(varname = "variable", dataname = "data", selected = c(posixct[2], posixct[10])))
+  )
+  testthat::expect_true(shiny::isolate(filter_state$is_any_filtered()))
+})
+
 testthat::test_that("is_any_filtered returns TRUE when enabled", {
-  shiny::reactiveConsole(TRUE)
-  on.exit(shiny::reactiveConsole(FALSE))
   testfs <- R6::R6Class(
     classname = "testfs",
     inherit = DatetimeFilterState,
@@ -250,17 +230,16 @@ testthat::test_that("is_any_filtered returns TRUE when enabled", {
       enable = function() private$enable()
     )
   )
-  datetime_seq <- seq(Sys.time() - 120, Sys.time(), 60)
-  fs <- testfs$new(datetime_seq, varname = "x", dataname = "data")
-  fs$set_state(filter_var(selected = datetime_seq[1:2], keep_na = TRUE, varname = "x", dataname = "data"))
-  fs$disable()
-  fs$enable()
-  testthat::expect_true(fs$is_any_filtered())
+  fs <- testfs$new(posixct, dataname = "data", varname = "variable")
+  fs$set_state(filter_var(dataname = "data", varname = "variable", selected = posixct[1:2], keep_na = TRUE))
+  testthat::expect_true(shiny::isolate(fs$is_any_filtered()))
+  shiny::isolate(fs$disable())
+  testthat::expect_false(shiny::isolate(fs$is_any_filtered()))
+  shiny::isolate(fs$enable())
+  testthat::expect_true(shiny::isolate(fs$is_any_filtered()))
 })
 
 testthat::test_that("is_any_filtered reacts to choices", {
-  shiny::reactiveConsole(TRUE)
-  on.exit(shiny::reactiveConsole(FALSE))
   testfs <- R6::R6Class(
     classname = "testfs",
     inherit = DatetimeFilterState,
@@ -269,9 +248,8 @@ testthat::test_that("is_any_filtered reacts to choices", {
       enable = function() private$enable()
     )
   )
-  datetime_seq <- seq(Sys.time() - 120, Sys.time(), 60)
-  fs <- testfs$new(datetime_seq, varname = "x", dataname = "data", choices = datetime_seq[c(1, 2)])
-  testthat::expect_true(fs$is_any_filtered())
-  fs <- testfs$new(datetime_seq, varname = "x", dataname = "data", choices = datetime_seq[c(1, 3)])
-  testthat::expect_false(fs$is_any_filtered())
+  fs <- testfs$new(posixct, dataname = "data", varname = "variable")
+  testthat::expect_false(shiny::isolate(fs$is_any_filtered()))
+  fs <- testfs$new(posixct, dataname = "data", varname = "variable", choices = posixct[c(1, 3)])
+  testthat::expect_true(shiny::isolate(fs$is_any_filtered()))
 })
