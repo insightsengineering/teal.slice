e9646dd0348ed5315824f54b318049d56d7b36fc
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-04-08 18:57:58 +0200

    review tests for init_filter_state

--- tests/testthat/test-init_filter_state.R
+++ tests/testthat/test-init_filter_state.R
@@ -1,89 +1,137 @@
-testthat::test_that("init_filter_state accepts a string, not name as varname", {
-  testthat::expect_no_error(init_filter_state(7, varname = "test", dataname = "data"))
-  testthat::expect_error(init_filter_state(7, varname = quote(test)))
+
+# argument checks ----
+testthat::test_that("init_filter_state checks arguments", {
+  testthat::expect_error(init_filter_state(), "argument \"dataname\" is missing")
+  testthat::expect_error(init_filter_state(dataname = "data"), "argument \"varname\" is missing")
+  testthat::expect_error(init_filter_state(dataname = "data", varname = "variable"), "argument \"x\" is missing")
+
+  testthat::expect_error(
+    init_filter_state(7, dataname = quote(pi), varname = "variable"),
+    "Assertion on 'dataname' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(7, dataname = call("data"), varname = "variable"),
+    "Assertion on 'dataname' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(7, dataname = "data", varname = quote(pi)),
+    "Assertion on 'varname' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(7, dataname = "data", varname = call("test")),
+    "Assertion on 'varname' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(x = 7, dataname = "data", varname = "variable", x_reactive = NULL),
+    "Assertion on 'x_reactive' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(x = 7, dataname = "data", varname = "variable", keep_na = "TRUE"),
+    "Assertion on 'keep_na' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(x = 7, dataname = "data", varname = "variable", keep_inf = "TRUE"),
+    "Assertion on 'keep_inf' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(x = 7, dataname = "data", varname = "variable", fixed = NULL),
+    "Assertion on 'fixed' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(x = 7, dataname = "data", varname = "variable", disabled = NULL),
+    "Assertion on 'disabled' failed"
+  )
+  testthat::expect_error(
+    init_filter_state(x = 7, dataname = "data", varname = "variable", extract_type = "other"),
+    "Assertion on 'extract_type' failed"
+  )
 })
 
-testthat::test_that("init_filter_state accepts a character vector of length 0 or 1 as varlabel", {
-  testthat::expect_no_error(init_filter_state(7, varname = "test", varlabel = "test", dataname = "data"))
-  testthat::expect_no_error(init_filter_state(7, varname = "test", varlabel = character(0), dataname = "data"))
-})
-
-testthat::test_that("init_filter_state accepts character as extract_type", {
-  testthat::expect_no_error(
-    init_filter_state(7, varname = "test", extract_type = character(0), dataname = "test")
-  )
-
-  testthat::expect_no_error(
-    init_filter_state(7, varname = "test", dataname = "test", extract_type = "list")
-  )
-  testthat::expect_no_error(
-    init_filter_state(7, varname = "test", dataname = "test", extract_type = "matrix")
-  )
-})
-
+# return type ----
 testthat::test_that("init_filter_state returns an EmptyFilterState if all values provided are NA", {
-  testthat::expect_true(is(init_filter_state(NA, varname = "test", dataname = "data"), "EmptyFilterState"))
+  testthat::expect_s3_class(init_filter_state(NA, dataname = "data", varname = "variable"), "EmptyFilterState")
 })
 
-testthat::test_that("init_filter_state returns a ChoicesFilterState if passed a longer numeric", {
+testthat::test_that("init_filter_state returns a ChoicesFilterState if passed a numeric of length 1", {
   numbers <- 1
-  testthat::expect_s3_class(init_filter_state(numbers, varname = "test", dataname = "data"), "ChoicesFilterState")
+  testthat::expect_s3_class(init_filter_state(numbers, dataname = "data", varname = "variable"), "ChoicesFilterState")
 })
 
-testthat::test_that("init_filter_state returns a ChoicesFilterState if passed a longer numeric", {
-  numbers <- 1:getOption("teal.threshold_slider_vs_checkboxgroup") + 1
-  testthat::expect_s3_class(init_filter_state(numbers, varname = "test", dataname = "data"), "RangeFilterState")
+testthat::test_that("init_filter_state returns a RangeFilterState if passed a longer numeric", {
+  numbers <- seq(1, length.out = getOption("teal.threshold_slider_vs_checkboxgroup") + 1)
+  testthat::expect_s3_class(init_filter_state(numbers, dataname = "data", varname = "variable"), "RangeFilterState")
 })
 
 testthat::test_that("init_filter_state returns a ChoicesFilterState object if passed a Date object of length 1", {
   dates <- seq(as.Date("1990/01/01"), by = 1, length.out = 1)
-  testthat::expect_s3_class(init_filter_state(dates, varname = "test", dataname = "data"), "ChoicesFilterState")
+  testthat::expect_s3_class(init_filter_state(dates, dataname = "data", varname = "variable"), "ChoicesFilterState")
 })
 
 testthat::test_that("init_filter_state returns a DateFilterState object if passed longer a Date object", {
   dates <- seq(as.Date("1990/01/01"), by = 1, length.out = getOption("teal.threshold_slider_vs_checkboxgroup") + 1)
-  testthat::expect_s3_class(init_filter_state(dates, varname = "test", dataname = "data"), "DateFilterState")
+  testthat::expect_s3_class(init_filter_state(dates, dataname = "data", varname = "variable"), "DateFilterState")
 })
 
-testthat::test_that("init_filter_state returns a DatetimeFilterState object if passed
+testthat::test_that("init_filter_state returns a ChoicesFilterState object if passed
   a POSIXct or POSIXlt of length 1", {
   dates <- seq(as.Date("1990/01/01"), by = 1, length.out = 1)
   testthat::expect_s3_class(
-    init_filter_state(as.POSIXct(dates), varname = "test", dataname = "data"), "ChoicesFilterState")
+    init_filter_state(as.POSIXct(dates), dataname = "data", varname = "variable"), "ChoicesFilterState")
   testthat::expect_s3_class(
-    init_filter_state(as.POSIXlt(dates), varname = "test", dataname = "data"), "ChoicesFilterState")
+    init_filter_state(as.POSIXlt(dates), dataname = "data", varname = "variable"), "ChoicesFilterState")
 })
 
 testthat::test_that("init_filter_state returns a DatetimeFilterState object if passed
   a longer POSIXct or POSIXlt", {
   dates <- seq(as.Date("1990/01/01"), by = 1, length.out = getOption("teal.threshold_slider_vs_checkboxgroup") + 1)
   testthat::expect_s3_class(
-    init_filter_state(as.POSIXct(dates), varname = "test", dataname = "data"), "DatetimeFilterState")
+    init_filter_state(as.POSIXct(dates), dataname = "data", varname = "variable"), "DatetimeFilterState")
   testthat::expect_s3_class(
-    init_filter_state(as.POSIXlt(dates), varname = "test", dataname = "data"), "DatetimeFilterState")
+    init_filter_state(as.POSIXlt(dates), dataname = "data", varname = "variable"), "DatetimeFilterState")
 })
 
-testthat::test_that("init_filter_state returns a RangeFilterState if passed a numeric array containing Inf", {
-  testthat::expect_no_error(fs <- init_filter_state(c(1, 2, 3, 4, Inf), varname = "test", dataname = "data"))
-  testthat::expect_true(is(fs, "RangeFilterState"))
+testthat::test_that("init_filter_state returns a RangeFilterState if passed a numeric vector containing Inf", {
+  testthat::expect_no_error(fs <- init_filter_state(c(1, 2, 3, 4, Inf), dataname = "data", varname = "variable"))
+  testthat::expect_s3_class(fs, "RangeFilterState")
 })
 
 testthat::test_that("init_filter_state returns a ChoicesFilterState if passed fewer than five non-NA elements", {
-  testthat::expect_no_error(fs <- init_filter_state(c(1, 2, 3, 4, NA), varname = "test", dataname = "data"))
-  testthat::expect_true(is(fs, "ChoicesFilterState"))
+  testthat::expect_no_error(fs <- init_filter_state(c(1, 2, 3, 4, NA), dataname = "data", varname = "variable"))
+  testthat::expect_s3_class(fs, "ChoicesFilterState")
 })
 
-testthat::test_that("init_filter_state returns a ChoicesFilterState, if passed a character array", {
-  testthat::expect_no_error(fs <- init_filter_state(c("a"), varname = "test", dataname = "data"))
-  testthat::expect_true(is(fs, "ChoicesFilterState"))
+testthat::test_that("init_filter_state returns a ChoicesFilterState, if passed a character vector of any length", {
+  testthat::expect_no_error(fs <- init_filter_state("a", dataname = "data", varname = "variable"))
+  testthat::expect_s3_class(fs, "ChoicesFilterState")
+
+  chars <- replicate(
+    n = getOption("teal.threshold_slider_vs_checkboxgroup") + 1,
+    paste(sample(letters, size = 10, replace = TRUE), collapse = "")
+  )
+  testthat::expect_no_error(fs <- init_filter_state("a", dataname = "data", varname = "variable"))
+  testthat::expect_s3_class(fs, "ChoicesFilterState")
 })
 
-testthat::test_that("init_filter_state return a LogicalFilterState, if passed a logical array", {
-  testthat::expect_no_error(fs <- init_filter_state(c(TRUE), varname = "test", dataname = "data"))
-  testthat::expect_true(is(fs, "LogicalFilterState"))
+testthat::test_that("init_filter_state returns a ChoicesFilterState, if passed a factor of any length", {
+  testthat::expect_no_error(fs <- init_filter_state(factor("a"), dataname = "data", varname = "variable"))
+  testthat::expect_s3_class(fs, "ChoicesFilterState")
+
+  chars <- factor(
+    replicate(
+      n = getOption("teal.threshold_slider_vs_checkboxgroup") + 1,
+      paste(sample(letters, size = 10, replace = TRUE), collapse = "")
+    )
+  )
+  testthat::expect_no_error(fs <- init_filter_state("a", dataname = "data", varname = "variable"))
+  testthat::expect_s3_class(fs, "ChoicesFilterState")
+})
+
+testthat::test_that("init_filter_state return a LogicalFilterState, if passed a logical vector", {
+  testthat::expect_no_error(fs <- init_filter_state(c(TRUE), dataname = "data", varname = "variable"))
+  testthat::expect_s3_class(fs, "LogicalFilterState")
 })
 
 testthat::test_that("init_filter_state default accepts a list", {
-  fs <- init_filter_state(list(1, 2, 3), varname = "test", dataname = "data")
-  testthat::expect_true(inherits(fs, "FilterState"))
+  fs <- init_filter_state(list(1, 2, 3), dataname = "data", varname = "variable")
+  testthat::expect_s3_class(fs, "FilterState")
 })
