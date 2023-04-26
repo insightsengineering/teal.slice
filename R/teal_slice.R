32afbe7f564ba64ef5c9b2469b16c4deb130aa7d
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-04-26 10:35:22 +0200

    fix typo in docs

--- R/teal_slice.R
+++ R/teal_slice.R
@@ -67,7 +67,7 @@
 #' @param show_all `logical(1)` specifying whether NULL elements should also be printed
 #' @param tss `teal_slices`
 #' @param field `character(1)` name of `teal_slice` element
-#' @param expr `character` string representing and expression that evaluates to a single `logical`;
+#' @param expr `character` string representing an expression that evaluates to a single `logical`;
 #'             will be evaluated in individual `teal_slice` objects
 #' @param ... for `filter_var` any number of additional fields given as `name:value` pairs\cr
 #'            for `filter_settings` any number of `teal_slice` objects\cr
