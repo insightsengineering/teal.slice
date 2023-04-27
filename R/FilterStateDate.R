a235f8343180b9c8aceb62774cd23a2374abe770
Andrew Bates <andrew.bates@atorusresearch.com>
2023-04-27 14:42:34 -0700

    value display in summary for data filter

--- R/FilterStateDate.R
+++ R/FilterStateDate.R
@@ -476,7 +476,10 @@ DateFilterState <- R6::R6Class( # nolint
       min <- selected[1]
       max <- selected[2]
       tagList(
-        tags$span(paste0(min, " - ", max)),
+        tags$span(
+          class = "filter-card-summary-value",
+          paste0(min, " - ", max)
+        ),
         tags$span(
           class = "filter-card-summary-controls",
           if (isTRUE(private$get_keep_na()) && private$na_count > 0) {
