84477f2c404c5247f9109af0a32651c94a1041f1
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-05-27 00:32:43 +0530

    Update R/FilterStateLogical.R
    Co-authored-by: Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
    Signed-off-by: kartikeya kirar <kirar.kartikeya1@gmail.com>

--- R/FilterStateLogical.R
+++ R/FilterStateLogical.R
@@ -266,7 +266,7 @@ LogicalFilterState <- R6::R6Class( # nolint
         FALSE
       } else if (private$is_choice_limited) {
         TRUE
-      } else if (!setequal(private$get_selected(), private$choices)) {
+      } else if (isFALSE(all.equal(private$get_selected(), private$choices))) {
         TRUE
       } else if (isFALSE(private$get_keep_na()) && private$na_count > 0) {
         TRUE
