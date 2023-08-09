ffe055e07b30e12dc556b5930306846e6bcc375d
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-08-09 16:49:19 +0530

    Prevent to deselect any value in DateFilterState 429 (#433)
    this fixes: https://github.com/insightsengineering/teal.slice/issues/429
    
    I have updated the DateFilterState and incorporated restoration features
    for previous selections. Each start and end date is now individually
    assessed for more accurate notifications.
    
    The use of shinyWidgets::airDatepickerInput in DateTimefilterState poses
    a challenge, as it lacks a return value when cleared. This requires
    modifications to enable the observation of user actions and the
    reversion to the previous selection.
    
    Demo
    
    
    https://github.com/insightsengineering/teal.slice/assets/6700955/17b4e04a-2e8c-4518-a05e-522255d251ec
    
    ---------
    
    Co-authored-by: kartikeya <kartikeya.kirar@unicle.life>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- R/FilterStateDate.R
+++ R/FilterStateDate.R
@@ -343,12 +343,21 @@ DateFilterState <- R6::R6Class( # nolint
               logger::log_trace("DateFilterState$server@2 selection changed, id: { private$get_id() }")
               start_date <- input$selection[1]
               end_date <- input$selection[2]
-              if (start_date > end_date) {
+
+              if (is.na(start_date) || is.na(end_date) || start_date > end_date) {
+                updateDateRangeInput(
+                  session = session,
+                  inputId = "selection",
+                  start = private$get_selected()[1],
+                  end = private$get_selected()[2]
+                )
                 showNotification(
-                  "Start date must not be greater than the end date. Setting back to default values.",
+                  "Start date must not be greater than the end date. Setting back to previous value.",
                   type = "warning"
                 )
+                return(NULL)
               }
+
               private$set_selected(c(start_date, end_date))
             }
           )
