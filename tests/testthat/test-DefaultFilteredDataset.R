8a77d2bb0d0dfe6e92125147aa9803399ab33fd3
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-12-11 16:24:22 +0100

    add unit tests for the new class

---
+++ tests/testthat/test-DefaultFilteredDataset.R
@@ -0,0 +1,52 @@
+# initialize ----
+testthat::test_that("constructor accepts all types of datasets", {
+  testthat::expect_no_error(FilteredData$new(list("logical" = c(TRUE, FALSE))))
+  testthat::expect_no_error(FilteredData$new(list("integer" = 1:10)))
+  testthat::expect_no_error(FilteredData$new(list("numeric" = 1:10 * 1)))
+  testthat::expect_no_error(FilteredData$new(list("character" = letters)))
+  testthat::expect_no_error(FilteredData$new(list("factor" = as.factor(letters))))
+  testthat::expect_no_error(FilteredData$new(list("list" = as.list(letters))))
+  testthat::expect_no_error(FilteredData$new(list("function" = function() letters)))
+  testthat::expect_no_error(FilteredData$new(list("array" = array(1:27, dim = c(3, 3, 3)))))
+})
+
+# format ----
+testthat::test_that("format dispalys object name and class", {
+  fds <- DefaultFilteredDataset$new(letters, "character")
+  testthat::expect_identical(
+    fds$format(),
+    "DefaultFilteredDataset:\n\"character\": character"
+  )
+})
+
+# get_call ----
+
+# get_filter_state ----
+testthat::test_that("get_filter_state returns NULL with a warning", {
+  fds <- DefaultFilteredDataset$new(letters, "character")
+  testthat::expect_null(
+    testthat::expect_warning(fds$get_filter_state(), "does not have state")
+  )
+})
+# set_filter_state ----
+testthat::test_that("set_filter_state returns NULL with a warning", {
+  fds <- DefaultFilteredDataset$new(letters, "character")
+  tss <- teal_slices(teal_slice("letters", "letter"))
+  testthat::expect_null(
+    testthat::expect_warning(fds$set_filter_state(tss), "cannot set state")
+  )
+})
+# clear_filter_states ----
+testthat::test_that("clear_filter_state returns NULL with a warning", {
+  fds <- DefaultFilteredDataset$new(letters, "character")
+  testthat::expect_null(
+    testthat::expect_warning(fds$clear_filter_states(), "does not have filter states")
+  )
+})
+# get_filter_overview ----
+testthat::test_that("GET_FILTER_OVERVIEW returns NULL with a warning", {
+  fds <- DefaultFilteredDataset$new(letters, "character")
+  testthat::expect_null(
+    fds$get_filter_overview()
+  )
+})
