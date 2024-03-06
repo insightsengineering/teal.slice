23965bff6f0b179dff83e92efdbca640861d0cc3
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-03-06 17:02:01 +0100

    add clear_aliases.R script

---
+++ inst/clear_aliases.R
@@ -0,0 +1,12 @@
+# run this script before every release
+
+Rd_db <- getFromNamespace("Rd_db", "tools")
+Rd_contents <- getFromNamespace("Rd_contents", "tools")
+Rd_list <- Rd_db(dir = ".")
+Rd <- Rd_contents(Rd_list)
+
+Rd$Aliases <- ifelse(Rd$Name %in% c("teal_slice", "teal_slices"), Rd$Name, Rd$Aliases)
+
+Meta_dir <- file.path("inst", "Meta")
+if (!dir.exists(Meta_dir)) dir.create(Meta_dir)
+saveRDS(Rd, file = file.path(Meta_dir, "Rd.rds"))
