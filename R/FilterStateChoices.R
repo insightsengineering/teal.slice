b7a84e0faebaee4448c3f1f827861dab46e5dad2
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2024-04-23 10:23:56 +0200

    don't subset private$x (it makes a new copy)

--- R/FilterStateChoices.R
+++ R/FilterStateChoices.R
@@ -240,13 +240,13 @@ ChoicesFilterState <- R6::R6Class( # nolint
     # Checks validity of the choices, adjust if neccessary and sets the flag for the case where choices
     #  are limited by default from the start.
     set_choices = function(choices) {
-      ordered_counts <- table(as.character(private$x))
+      ordered_counts <- table(private$x)
       possible_choices <- names(ordered_counts)
       if (is.null(choices)) {
         choices <- possible_choices
       } else {
         choices <- as.character(choices)
-        choices_adjusted <- choices[choices %in% unique(private$x)]
+        choices_adjusted <- choices[choices %in% possible_choices]
         if (length(setdiff(choices, choices_adjusted)) > 0L) {
           warning(
             sprintf(
@@ -269,8 +269,6 @@ ChoicesFilterState <- R6::R6Class( # nolint
       private$set_choices_counts(unname(ordered_counts[choices]))
       private$set_is_choice_limited(possible_choices, choices)
       private$teal_slice$choices <- choices
-      private$x <- private$x[(private$x %in% private$get_choices()) | is.na(private$x)]
-      if (is.factor(private$x)) private$x <- droplevels(private$x)
       invisible(NULL)
     },
     # @description
@@ -285,15 +283,6 @@ ChoicesFilterState <- R6::R6Class( # nolint
     set_choices_counts = function(choices_counts) {
       private$choices_counts <- choices_counts
       invisible(NULL)
-    },
-    # @description
-    # Checks how many counts of each choice is present in the data.
-    get_choices_counts = function() {
-      if (!is.null(private$x_reactive)) {
-        table(factor(private$x_reactive(), levels = private$get_choices()))
-      } else {
-        NULL
-      }
     },
     # @description
     # Checks whether the input should be rendered as a checkboxgroup/radiobutton or a drop-down.
@@ -346,7 +335,9 @@ ChoicesFilterState <- R6::R6Class( # nolint
       isolate({
         countsmax <- private$choices_counts
         countsnow <- if (!is.null(private$x_reactive())) {
-          unname(table(factor(private$x_reactive(), levels = private$get_choices())))
+          unname(
+            table(private$x_reactive())[private$get_choices()]
+          )
         }
 
         ui_input <- if (private$is_checkboxgroup()) {
@@ -424,7 +415,9 @@ ChoicesFilterState <- R6::R6Class( # nolint
             logger::log_trace("ChoicesFilterState$server_inputs@1 updating count labels, id: { private$get_id() }")
 
             countsnow <- if (!is.null(private$x_reactive())) {
-              unname(table(factor(non_missing_values(), levels = private$get_choices())))
+              unname(
+                table(non_missing_values())[private$get_choices()]
+              )
             }
 
             # update should be based on a change of counts only
@@ -549,7 +542,9 @@ ChoicesFilterState <- R6::R6Class( # nolint
 
           output$selection <- renderUI({
             countsnow <- if (!is.null(private$x_reactive())) {
-              unname(table(factor(private$x_reactive(), levels = private$get_choices())))
+              unname(
+                table(private$x_reactive())[private$get_choices()]
+              )
             }
             countsmax <- private$choices_counts
 
