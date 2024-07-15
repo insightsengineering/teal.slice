7a4a60a4dc59d2d101ea6bf623cf536d94b761f9
vedhav <vedhaviyash4@gmail.com>
2024-07-15 19:14:57 +0530

    chore: use param name

--- R/FilteredData.R
+++ R/FilteredData.R
@@ -579,7 +579,7 @@ FilteredData <- R6::R6Class( # nolint
               isolate(active_datanames()),
               function(dataname) {
                 fdataset <- private$get_filtered_dataset(dataname)
-                fdataset$ui_active(id = ns(dataname), private$allow_add)
+                fdataset$ui_active(id = ns(dataname), allow_add = private$allow_add)
               }
             )
           )
