aef12a6853aa64d6b0a0a4e03e208e2a48558dd1
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-10-15 14:58:13 +0100

    fix: allow non-standard datanames in filter data

--- R/utils.R
+++ R/utils.R
@@ -1,28 +1,3 @@
-#' Test whether variable name can be used within `Show R Code`
-#'
-#' Variable names containing spaces are problematic and must be wrapped in backticks.
-#' Also, they should not start with a number as `R` may silently make it valid by changing it.
-#' Therefore, we only allow alphanumeric characters with underscores.
-#' The first character of the `name` must be an alphabetic character and can be followed by alphanumeric characters.
-#'
-#' @md
-#'
-#' @param name (`character`) vector of names to check
-#' @return Returns `NULL` or raises error.
-#' @keywords internal
-#'
-check_simple_name <- function(name) {
-  checkmate::assert_character(name, min.len = 1, any.missing = FALSE)
-  if (!grepl("^[[:alpha:]][a-zA-Z0-9_]*$", name, perl = TRUE)) {
-    stop(
-      "name '",
-      name,
-      "' must only contain alphanumeric characters (with underscores)",
-      " and the first character must be an alphabetic character"
-    )
-  }
-}
-
 #' Include `JS` files from `/inst/js/` package directory to application header
 #'
 #' `system.file` should not be used to access files in other packages, it does
