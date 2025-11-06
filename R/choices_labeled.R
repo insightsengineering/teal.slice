5c9be5f953a612a92be372a2c66df98063748051
Dawid Kaledkowski <dawid.kaledkowski@gmail.com>
2025-11-06 10:54:05 +0100

    - remove unused parameter choices_labeled(subset) - simplify assertions

--- R/choices_labeled.R
+++ R/choices_labeled.R
@@ -11,50 +11,16 @@
 #' @param choices (`character` or `numeric` or `logical`) vector
 #' @param labels (`character`) vector containing labels to be applied to `choices`. If `NA` then
 #' "Label Missing" will be used.
-#' @param subset a vector that is a subset of `choices`. This is useful if
-#'   only a few variables need to be named. If this argument is used, the returned vector will
-#'   match its order.
 #' @param types vector containing the types of the columns.
 #'
 #' @return A named character vector.
 #'
 #' @keywords internal
 #'
-choices_labeled <- function(choices, labels, subset = NULL, types = NULL) {
-  if (is.factor(choices)) {
-    choices <- as.character(choices)
-  }
-
-  stopifnot(
-    is.character(choices) ||
-      is.numeric(choices) ||
-      is.logical(choices) ||
-      (length(choices) == 1 && is.na(choices))
-  )
-
-  if (is.factor(labels)) {
-    labels <- as.character(labels)
-  }
-
-  checkmate::assert_character(labels[!is.na(labels)], any.missing = FALSE)
-  if (length(choices) != length(labels)) {
-    stop("length of choices must be the same as labels")
-  }
-  stopifnot(is.null(subset) || is.vector(subset))
-  stopifnot(is.null(types) || is.vector(types))
-
-  if (is.vector(types)) {
-    stopifnot(length(choices) == length(types))
-  }
-
-  if (!is.null(subset)) {
-    if (!all(subset %in% choices)) {
-      stop("all of subset variables must be in choices")
-    }
-    labels <- labels[choices %in% subset]
-    types <- types[choices %in% subset]
-    choices <- choices[choices %in% subset]
-  }
+choices_labeled <- function(choices, labels, types = NULL) {
+  checkmate::assert_character(choices)
+  checkmate::assert_character(labels[!is.na(labels)], len = length(choices))
+  checkmate::assert_character(types, len = length(choices), null.ok = TRUE)
 
   is_dupl <- duplicated(choices)
   choices <- choices[!is_dupl]
@@ -68,13 +34,6 @@ choices_labeled <- function(choices, labels, subset = NULL, types = NULL) {
     character(0)
   }
 
-  if (!is.null(subset)) {
-    ord <- match(subset, choices)
-    choices <- choices[ord]
-    raw_labels <- raw_labels[ord]
-    combined_labels <- combined_labels[ord]
-    types <- types[ord]
-  }
   choices <- structure(
     choices,
     names = combined_labels,
